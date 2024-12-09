48c6539208552ac643982c45343a7cbc3dc660e0
Davsmusk <da974125@gmail.com>
2024-12-09 15:39:56 -0600

    Add Render domain to ALLOWED_HOSTS

--- la_paca/settings.py
+++ la_paca/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-#-3b&y&7v9i4n+lvtzhl(-nal4x*n79j#p288weode$u*3l$oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fubuking.onrender.com']
+
 
 
 # Application definition
